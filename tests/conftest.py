dceb3d9d7213234f661de161029e2e2392eabb4b
KyriakosZorbas <15860064+KyriakosZorbas@users.noreply.github.com>
2020-07-24 09:15:33 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,11 @@
-import os
+def pytest_addoption(parser):
+    parser.addoption("--all", action="store_true", help="run all combinations")
+
+
+def pytest_generate_tests(metafunc):
+    if "param1" in metafunc.fixturenames:
+        if metafunc.config.getoption("all"):
+            end = 5
+        else:
+            end = 2
+        metafunc.parametrize("param1", range(end))
